ff94886a81ccc65958c4a34c5d2aaf74ac2f70c0
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2022-04-01 10:05:08 -0400

    Delete JavaScript/307 directory

--- JavaScript/307/script2.js
+++
@@ -1,7 +0,0 @@
-function randWord() {
-  var num = Math.floor(Math.random() * 10);
-  var words = ['mechanism', 'epiphany', 'harvest', 'government', 'exhibition', 'definite', 'excuse', 'topple', 'uncertainty', 'suspect']
-  return words[num];
-}
-
-document.write(randWord());
