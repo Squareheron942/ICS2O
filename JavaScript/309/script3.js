ec00b152dcfcf4f2f5b5465a055757d95eab2248
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2022-04-01 10:05:30 -0400

    Delete JavaScript/309 directory

--- JavaScript/309/script3.js
+++
@@ -1,7 +0,0 @@
-function countWords() {
-  var str = document.getElementById('sntcIn').value;
-  str = str.replace(/(^\s*)|(\s*$)/gi,"");
-  str = str.replace(/[ ]{2,}/gi," ");
-  str = str.replace(/\n /,"\n");
-  document.getElementById('wrdCount').innerHTML = str.split(' ').length;
-}
